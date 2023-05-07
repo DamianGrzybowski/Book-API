e4e29efc1c30f7184e45d7632126fd104ef47922
Damian Grzybowski <d.grzybowski96@gmail.com>
2023-05-07 13:30:33 +0200

    Create new book form and added validation

--- src/main/java/pl/coderslab/entity/Book.java
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -9,6 +9,9 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -17,22 +20,37 @@ import javax.transaction.Transactional;
 @Transactional
 @Table(name = "books")
 public class Book {
+    private static final String NOT_NULL_MESSAGE = "The field cannot be empty";
+    private static final String SIZE_MESSAGE = "Please enter text between 3 and 50 characters";
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @NotNull(message = NOT_NULL_MESSAGE)
+    @Pattern(regexp = "^(978|979)[\\- ]?\\d{1,5}[\\- ]?\\d{1,7}[\\- ]?\\d{1,6}[\\- ]?\\d$", message = "Please enter correct ISBN format")
     @Column(name = "isbn")
     private String isbn;
 
+    @NotNull(message = NOT_NULL_MESSAGE)
+    @Size(min = 3, max = 50, message = SIZE_MESSAGE)
     @Column(name = "title")
     private String title;
 
+    @NotNull(message = NOT_NULL_MESSAGE)
+    @Size(min = 3, max = 50, message = SIZE_MESSAGE)
     @Column(name = "author")
     private String author;
 
+    @NotNull(message = NOT_NULL_MESSAGE)
+    @Size(min = 3, max = 50, message = SIZE_MESSAGE)
     @Column(name = "publisher")
     private String publisher;
 
+    @NotNull(message = NOT_NULL_MESSAGE)
+    @Size(min = 3, max = 50, message = SIZE_MESSAGE)
     @Column(name = "type")
     private String type;
 
