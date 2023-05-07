4dd719fadee7ceb2d44a3cb0a56d9d0bf8b29562
Damian Grzybowski <d.grzybowski96@gmail.com>
2023-05-07 12:24:35 +0200

    Fixes

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,5 +1,6 @@
 package pl.coderslab.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -9,19 +10,11 @@ import pl.coderslab.entity.Book;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
-    private MockBookService bookService;
+    private final MockBookService bookService;
 
-    public BookController(MockBookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @RequestMapping("/helloBook")
-    public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java",
-                "Bruce Eckel", "Helion", "programming");
-    }
 
     @GetMapping("")
     public List<Book> getAllBooks() {
@@ -30,7 +23,7 @@ public class BookController {
 
     @GetMapping("/{id}")
     public Book bookById(@PathVariable Long id) {
-        return bookService.getBook(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found"));
+        return bookService.getBook(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found"));
     }
 
     @PostMapping("")
